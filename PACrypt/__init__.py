65b40031a80aa4f51ac071ced0aff0f9ae63c795
nikitt-code <nikitt.russia@gmail.com>
2021-01-19 23:04:40 +0500

    Beta. 0.0.2.1

--- PACrypt/__init__.py
+++ PACrypt/__init__.py
@@ -4,21 +4,26 @@ class Init:
         return
 
     def phone(self, phone: int) -> int:
-        phstr = str(phone)
-        step = int(phstr[0])
-        for x in range(len(phstr)):
-            if not x + 1 == len(phstr):
-                step = (step ** int(phstr[x + 1]) // int(phstr[x + 1]))
-        return step % (phone ** 10)
+        try:
+            phstr = str(phone)
+            step = int(phstr[0])
+            for x in range(len(phstr)):
+                if not x + 1 == len(phstr):
+                    step = (step ** int(phstr[x + 1]) // int(phstr[x + 1]))
+            return step % (phone ** 10)
+        except:
+            return 0
 
     def password(self, line, step) -> int:
-        line = line.lower()
-        hash_sum = 1
-        for x in range(len(line)):
-            curr_let = line[x]
-            curr_let_num = ord(curr_let) - 96
-            hash_sum *= curr_let_num + ((curr_let_num ** step) % step) ** curr_let_num
-        while hash_sum % 10 == 0:
-            hash_sum = hash_sum // 10
-        return hash_sum
-
+        try:
+            line = line.lower()
+            hash_sum: int = 1
+            for x in range(len(line)):
+                curr_let = line[x]
+                curr_let_num = ord(curr_let) - 96
+                hash_sum *= curr_let_num + ((curr_let_num ** step) % step) ** curr_let_num
+            while hash_sum % 10 == 0:
+                hash_sum = hash_sum // 10
+            return hash_sum
+        except:
+            return 0

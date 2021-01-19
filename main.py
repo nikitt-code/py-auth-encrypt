32c81843a82588753e2079adf7c76e429d05ef91
nikitt-code <nikitt.russia@gmail.com>
2021-01-19 22:37:07 +0500

    Beta. 0.0.1.1

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 def str2int(line: str, step: int = 3) -> int:
     line = line.lower()
-    lets = {
-        "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8,
-        "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16,
-        "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23,
-        "x": 24, "y": 25, "z": 26
-    }
     hash_sum = 1
     for x in range(len(line)):
         curr_let = line[x]
-        hash_sum *= lets[curr_let] + ((lets[curr_let] ** step) % step) ** lets[curr_let]
+        curr_let_num = ord(curr_let)-96
+        hash_sum *= curr_let_num + ((curr_let_num ** step) % step) ** curr_let_num
     while hash_sum % 10 == 0:
         hash_sum = hash_sum // 10
     return hash_sum
